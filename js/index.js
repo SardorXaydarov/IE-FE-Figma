84a5bb4158dcb4dfd476f77025ad9e9fd48a3e47
SardorXaydarov <xaydarovsardoryt@gmail.com>
2024-01-30 23:59:08 +0500

    tovar.html added and finished

--- js/index.js
+++ js/index.js
@@ -1,15 +1,32 @@
 let time = 86400;
-let hour= 3600
+let hour = 3600
 const countDownEl = document.getElementById("sale-time");
 
 setInterval(updateDown, 1000);
 
-function updateDown(){
-    const hours= Math.floor(time/3600)
-    const minutes = Math.floor(hour/60);
-    let seconds = time%60;
-    seconds = seconds < 10 ? "0" + seconds :seconds;
-    countDownEl.innerHTML= `${hours} : ${minutes} : ${seconds}`
+function updateDown() {
+    const hours = Math.floor(time / 3600)
+    const minutes = Math.floor(hour / 60);
+    let seconds = time % 60;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    countDownEl.innerHTML = `${hours} : ${minutes} : ${seconds}`
     time--;
     hour--;
 }
+
+
+const minus = document.querySelector('.minus'),
+    number = document.querySelector('.number'),
+    plus = document.querySelector('.plus');
+
+let a = 1;
+plus.addEventListener('click', ()=>{
+    a++;
+    number.innerText= a;
+})
+minus.addEventListener("click", ()=>{
+    if(a>1){
+        a--;
+        number.innerText=a;
+    }
+})
